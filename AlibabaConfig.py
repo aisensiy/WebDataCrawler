049f0d2d480152f0295bbee62b1e645433833f1f
aisensiy <aisensiy@163.com>
2011-04-20 18:18:47 +0800

    I add lots of comments for the files and add README file On branch master 	modified:   AlibabaConfig.py 	new file:   README 	modified:   crawler.py

--- AlibabaConfig.py
+++ AlibabaConfig.py
@@ -1,22 +1,34 @@
 # -*- coding: utf-8 -*-
-
+"""
+Here is the config file for crawling alibaba agriculture information.
+"""
+# give the encoding of web page
 encode = 'gb2312'
+# the directory of log file, the log file can aviod repeat download
+# when you restart the programe
 logfile = r'D:\python_sth\test\html\log.log'
+# This is where you download the web pages
 workdir = r'D:\python_sth\test\html'
+# config is a list in python, every item of it is a dictionary. The
+# config help create the LinkMap.
 config = [
           {
            'url':'http://page.china.alibaba.com/buy/trade/1.html',
+		   # selector is a css selector which is used to locate elements in the html
            'selector':'div#vi_fdev_main div#a3_tube_1 div.vi_frame00_04 div.hc_category ul li.dian',
+		   # keywords is a filter for elements get by selector
            'keywords':[],
+		   # it define the dir name, if the element selected is a a,
+		   # then the a's content will be the name if 'title' is not given
            'title':'css:a.fg2',
            },
            {
             'selector':'ul a',
+			# if no item can be found by the 'selector, default_selector can be used'
             'default_selector':'a.fg2',
             'process':lambda s: s.replace('n-y', 's-img_p-%d_cleanCookie-false_offset-10_pageSize-40_n-y'),
             },
             {
-#             'collapse': True,
              'selector':'#SearchList .offer .summary'
              }
           ]
--- crawler.py
+++ crawler.py
@@ -1,4 +1,26 @@
 # -*- coding: utf8 -*-
+"""
+This is the core of crawler(or even say this is the whole crawler
+right now, I will add new features for it.) and it's main creative
+feature is that it can crawler the web pages according to the config
+file and create a hierarchy like:
+	Item 1
+		Item 1.1
+			html files belong to Item 1.1
+		Item 1.2
+			html files belong to Item 1.2
+		...
+The hierarchy gives the info of classification which is quite
+estimable for our futhure work. 
+The crawle action right now contains two steps.
+step 1.	create the crawle hierarchy according to the config
+		dictionary, the hierarchy in the Python code is a big LinkMap,
+		a tree like data structure.
+step 2. crawle web pages under the structure of LinkMap. It knows
+		when to create a directory, when stop crawle and change to
+		another directory.
+Up to now, It works well on crawling Alibaba data. 
+"""
 from BeautifulSoup import *
 from soupselector import select
 from urllib2 import urlopen 
@@ -12,6 +34,7 @@ encode = 'gb2312'
 logfile = r'D:\python_sth\sina\zijin\log.log'
 # download file dir
 workdir = r'D:\python_sth\sina\zijin'
+
 config = [
           {
            'url':'http://roll.news.sina.com.cn/s_zjkywr_all/1/index.shtml',
@@ -24,8 +47,9 @@ config = [
           ]
 
 
-
+# here the 'keywords is used as a filter'
 def keyword_filter(soups, keywords):
+	"""as a filter to filte the soup which no keyword is included"""
     if keywords == None or len(keywords) == 0: return soups;
     new_soups = []
     for soup in soups:
@@ -36,6 +60,7 @@ def keyword_filter(soups, keywords):
     return new_soups
 
 def gettextonly(soup):
+	"""Get the text from html"""
     v = soup.string
     if v == None:
         c = soup.contents
@@ -48,12 +73,14 @@ def gettextonly(soup):
         return v.strip()
     
 def getlisttext(soups):
+	"""transform a list or a soup into plain text"""
     if type(soups) == list:
         resulttext = ''
         for soup in soups:
             resulttext += gettextonly(soup)
     else: return gettextonly(soups)
     
+# represent the hierarchy
 class LinkMap:
     def __init__(self, parent=None, children=None, soup=None, url=None, title=None, level=0, dir=None):
         self.level = level
@@ -62,10 +89,13 @@ class LinkMap:
         # soup and url always only one exists in the node
         self.soup = soup
         self.url = url
+		# title will be the name of the directory which is represented by the LinkMap object
         self.title = title
+		# the dir path 
         self.dir = dir
     
 def create_crawl_hierarchy(config):
+	"""the hierarchy is created according to the config"""
     map = LinkMap(
                   url=config[0]['url'],
                   dir=workdir
@@ -76,26 +106,40 @@ def create_crawl_hierarchy(config):
 def create_children(map):
     if map.level >= len(config) - 1:
         return
+
     current_config = config[map.level]
+
+	# here we create soup by url or soup from the parent 'map'
     if map.url != None:
         soup = BeautifulSoup(urlopen(map.url).read().decode(encode, 'ignore'))
     elif map.soup != None:
         soup = map.soup
+	# we use the 'selector' to create html fragments we called soups
     soups = select(soup, current_config['selector'])
     # use keywords as a filter
     if 'keywords' in current_config:
         soups = keyword_filter(soups, current_config['keywords'])
+		
     children = []
     for i in range(len(soups)):
+		# if the element is 'a' then we record url and make soup None,
+		# if the element is sth else, we make url None and soup the
+		# elements.
         if soups[i].name == 'a':
-#            print soups[i]['href'], soups[i].string
+#           print soups[i]['href'], soups[i].string
             url = soups[i]['href']
+			# use the content as the title
             title = soups[i].string
+			# if process is in the config, we will use it for url
+			# transformation.
             if 'process' in current_config: 
                 url = current_config['process'](url)
             soup = None
-
         else:
+			# if the name is not 'a', we may use the 'title' in
+			# config, if 'title' startswith 'css:', we will use the
+			# element's content as the title, or we use the 'title'
+			# config directly.
             if current_config['title'].startswith('css:'):
                 title_css = current_config['title'].split(':', 1)[1]
                 title = select(soups[i], title_css)[0].string
@@ -103,6 +147,8 @@ def create_children(map):
             url = None
             soup = soups[i]
 
+		# if 'collapse' is True then, it will not create a new dir
+		# with title as its name
         if 'collapse' not in current_config or current_config['collapse'] == False:
             dir = os.path.join(map.dir, title)
         else:
@@ -116,7 +162,8 @@ def create_children(map):
                                 level=map.level + 1, 
                                 dir=dir))
         
-    # when there is no sub links in the title, we should give another link
+    # when there is no sub links in the title, we should give another
+	# link, use the 'default_selector'
     if len(children) == 0 and 'default_selector' in current_config.keys():
         soups = select(soup, current_config['default_selector'])	    
         url = soups[0]['href']
@@ -137,6 +184,7 @@ def create_children(map):
         create_children(child)
 
 def crawl_pages(map):
+	"""Here we crawle pages according to the LinkMap"""
     if map.children == None:
         if map.url.find('%d') != -1:
             if not file_exist('%s\t%s' % (map.dir, map.url)):
@@ -169,17 +217,20 @@ def crawl_pages(map):
             
         
 def print_mapLink(map, indent):
+	"""print the linkmap for debug or test"""
     print '\t' * indent, map.level, map.title, map.url, map.dir
     if map.children != None:
         for child in map.children:
             print_mapLink(child, indent + 1)
             
 def file_exist(line):
+	"""test if the url or dir is downloaded"""
     if line in [elem.strip() for elem in open(logfile, 'r')]:
         return True
     return False
 
 def save_pagenates(leaf, limit= -1):
+	"""抓取有分页的url，他们的URL大多类似"""
     url_param = leaf.url
     index = 1
     url = url_param % (index)
@@ -225,6 +276,7 @@ def save_pagenates(leaf, limit= -1):
         url = url_param % (index)
         filename = os.path.join(leaf.dir, "%s.html" % (index))
         
+# not used
 def savePerpage(url, index, dir):
     filename = os.path.join(dir, str(index) + '.html')
     true_url = url % (index)
@@ -232,6 +284,7 @@ def savePerpage(url, index, dir):
     return page
 
 def save_page(url, dir):
+	"""download a single page"""
     if file_exist(dir + '\t' + url):
         print '%s is already downloaded.' % (url)
         return
